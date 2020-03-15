824d538e9320f9fb486a5302abb6e13070263b68
ardith lutfiawan <ardith.lutfiawan@gmail.com>
2020-03-16 02:05:03 +0700

    masuk pekan ke-4

---
+++ resources/Metode Penelitian/Mind Map.mm
@@ -0,0 +1,98 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Belum adanya penghubung antara mereka yang putus sekolah dengan orang yang mampu memberikan harta (donasi), tenaga (tutor/guru), dan fasilitas (fasilitator) pendidikan." FOLDED="false" ID="ID_1152272328" CREATED="1538951436028" MODIFIED="1539536365709" STYLE="oval">
+<font SIZE="16" BOLD="true"/>
+<hook NAME="MapStyle" zoom="0.605">
+    <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="8" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Aplikasi" POSITION="right" ID="ID_1470801418" CREATED="1538951991083" MODIFIED="1539536353842" HGAP_QUANTITY="139.9999962449075 pt" VSHIFT_QUANTITY="142.49999575316917 pt">
+<edge COLOR="#0000ff"/>
+<font NAME="Noto Sans CJK TC Bold" SIZE="18" BOLD="true"/>
+<node TEXT="Belum tersedia aplikasi berbasis web yang dapat memberikan berupa bantuan secara umum." ID="ID_362009593" CREATED="1538952963489" MODIFIED="1539536587640" HGAP_QUANTITY="-87.24999698251494 pt" VSHIFT_QUANTITY="-98.24999707192191 pt">
+<font NAME="Noto Sans CJK KR Regular" SIZE="16" BOLD="true"/>
+</node>
+<node TEXT="Fitur aplikasi yang tersedia hanya dapat memberikan donasi berupa uang pembiayaan pendidikan." ID="ID_503957012" CREATED="1538953138041" MODIFIED="1539536540138" HGAP_QUANTITY="40.999999195337324 pt" VSHIFT_QUANTITY="-2.9999999105930355 pt">
+<font NAME="Noto Sans CJK KR Regular" SIZE="16"/>
+</node>
+</node>
+<node TEXT="Sarana dan Prasarana" POSITION="left" ID="ID_791421690" CREATED="1538952069713" MODIFIED="1539536396971" HGAP_QUANTITY="117.49999691545972 pt" VSHIFT_QUANTITY="-14.999999552965178 pt">
+<edge COLOR="#00ff00"/>
+<font NAME="Noto Sans CJK TC Black" SIZE="18" BOLD="true"/>
+<node TEXT="Fasilitas kurang memadai untuk menunjang aktifitas pendidikan." ID="ID_1954129896" CREATED="1538952373975" MODIFIED="1539535505002" HGAP_QUANTITY="89.74999774247415 pt" VSHIFT_QUANTITY="-19.499999418854728 pt">
+<font NAME="Noto Sans CJK KR Regular" SIZE="16"/>
+</node>
+<node TEXT="Bantuan dari luar (non-pemerintah) sangat minim." ID="ID_306010797" CREATED="1538952500836" MODIFIED="1539535491772" HGAP_QUANTITY="105.49999727308759 pt" VSHIFT_QUANTITY="18.749999441206455 pt">
+<font NAME="Noto Sans CJK SC Regular" SIZE="16" BOLD="false"/>
+</node>
+</node>
+<node TEXT="Masyarakat" POSITION="right" ID="ID_1005477220" CREATED="1538952136894" MODIFIED="1539536365707" HGAP_QUANTITY="-228.24999278038763 pt" VSHIFT_QUANTITY="23.24999930709606 pt">
+<edge COLOR="#ff00ff"/>
+<font NAME="Noto Sans CJK TC Black" SIZE="18" BOLD="true"/>
+<node TEXT="Tidak mengetahui cara dan tempat untuk memberikan bantuan pendidikan" ID="ID_1754920857" CREATED="1538952812880" MODIFIED="1539536567107" HGAP_QUANTITY="-466.7499856725339 pt" VSHIFT_QUANTITY="-33.74999899417166 pt">
+<font NAME="Noto Sans CJK KR Regular" SIZE="16"/>
+</node>
+<node TEXT="Tidak memiliki informasi atas pendidikan bagi orang putus sekolah" ID="ID_1115607268" CREATED="1539535549644" MODIFIED="1539536340837" HGAP_QUANTITY="-462.9999857842927 pt" VSHIFT_QUANTITY="34.499998971819906 pt">
+<font NAME="Noto Sans CJK SC Regular" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
